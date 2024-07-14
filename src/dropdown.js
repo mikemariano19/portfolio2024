117b6e8c71405d14a2c83000ffcffc9656b50804
Mike Mariano <52498452+mikemariano19@users.noreply.github.com>
2024-07-14 22:01:01 +0800

    Fixing dropdown button

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,12 +1,16 @@
-const menu = document.getElementById('menu');
-const dropdown = document.getElementById('dropdown');
 
-dropdown.addEventListener('click', function(){
-    if(menu.classList.contains('hidden')){
-        menu.classList.remove('hidden')
-        console.log("clicked")
-    } else {
+
+    const menu = document.getElementById('menu');
+    const dropdown = document.getElementById('dropdown');
+    
+    dropdown.addEventListener('click', () => {
+        if(menu.classList.contains('hidden')) {
+            menu.classList.remove('hidden')
+        } else {
+            menu.classList.add('hidden')
+        }
+    })
+    
+    menu.addEventListener('click', () => {
         menu.classList.add('hidden')
-        console.log("unclicked")
-    }
-})
+    })
